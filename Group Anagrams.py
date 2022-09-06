176e3301350fc2b3857375911b9da7c83112783c
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2022-09-06 11:01:01 +0530

    group anagrams

---
+++ Group Anagrams.py
@@ -0,0 +1,12 @@
+import collections
+
+
+def GroupAnagram(strs):
+    answer=collections.defaultdict(list)
+    for word in strs:
+        answer[tuple(sorted(word))].append(word)
+    return answer.values()
+
+if __name__ in '__main__':
+    sequence=["eat","tea","tan","ate","nat","bat"]
+    print(GroupAnagram(sequence))
