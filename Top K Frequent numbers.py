221c4637a21dde6f119491a761e562cb64106eef
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2022-09-06 17:03:23 +0530

    L6len(nums)

--- Top K Frequent numbers.py
+++ Top K Frequent numbers.py
@@ -3,7 +3,7 @@ from typing import List
 def topKFrequent(nums: List[int], k: int) -> List[int]:
         
         count={}
-        freq=[[] for i in range(len(nums)+1)]
+        freq=[[] for i in range(len(nums))]
         for num in nums:
             count[num]= 1+count.get(num,0)#Counts no of times a number occured 
         for num,c in count.items():
@@ -18,4 +18,4 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
               
 
 if __name__=='__main__':
-     print(topKFrequent([0,0,0,0,1,6,1,2,2,6,2,6,2,6,6,6,7,8,65,456,243,5,34,5,2,2,2,2,2,2],5))
+     print(topKFrequent([0,0,0,0,1,6,1,2,2,6,2,6,2,6,6,6,6,66,6,6,6,6,6,6,6,6,7,8,65,456,243,5,34,5,2,2,2,2,2,2],5))
