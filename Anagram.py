7d240cb9e13bd6a698b795454f138f9ca2cdbacd
Ashvin <ashvinanilkumarsh@gmail.com>
2022-09-08 09:54:50 +0530

    Update Anagram.py

--- Anagram.py
+++ Anagram.py
@@ -1,5 +1,11 @@
+from collections import Counter
+
 s="anagram"
 t="nafgram"
-ss=sorted(s)
-st=sorted(t)
-print(ss==st)
+# dict1=Counter(s)
+# dict2=Counter(t)
+# print(dict1,dict2)
+count1,count2={},{}
+for i in range(len(s)):
+    count1[s[i]]=1+count1.get(s[i],0)
+print(count1)
