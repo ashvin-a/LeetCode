27435e147f34d83dc27914afee10eb7da098b81d
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2022-09-10 20:02:17 +0530

    adding encode decode

---
+++ EncodeDecode.py
@@ -0,0 +1,15 @@
+# def Encode(strs):
+#     res=""
+#     for s in strs:
+#         res+=str(len(s))+"#"+s
+#     return res                            
+
+# if __name__=='__main__':
+#     print(Encode("Hello  fldkfjgldfgk"))    
+
+# def Decode(str):
+nums=[1,3,45,6,7]
+numlist=sorted(nums)
+print(numlist)
+
+
